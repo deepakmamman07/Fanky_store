452d3543bf00849ddca7c67d0f5706613b0acab5
Deepak Mamman <deepakmamman08@gmail.com>
2024-06-14 11:01:25 +0530

    updating slider Components

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,56 +1,56 @@
-// import React from 'react'
-// import slider1 from "../assets/images/slider/slider1.jpg"
+import React from 'react';
+import Slider from 'react-slick';
+import '../../assets/plugins/slick/slick.css';
+import '../../assets/plugins/slick/slick-theme.css';
+import Background1 from "../assets/images/Funko/Funko-banner/Background1.jpg";
 
-// const Slider = () => {
-//   return (
-//     <>
-//     <div className="hero-slider">
-//   <div className="slider-item th-fullpage hero-area" style={{backgroundImage: url(${slider1})}}>
-//     <div className="container">
-//       <div className="row">
-//         <div className="col-lg-4 text-center">
-//           <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">DREAMY EXCLUSIVES</p>
-//           <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">GET YOUR SLEEP! </h1>
-//           <p> A Celebration is at Hand! Commemorate the 65th Anniversary of Disney's Sleeping Beauty with Funko & Loungefly Gear.
-//           <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" className="btn" href="#">Shop Collection</a></p>
-//         </div>
-//         <div className="col-lg-8 banner-img-1">
-//         </div>
-//       </div>
-//     </div>
-//   </div>
+const SliderComponent = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
-//   <div className="slider-item th-fullpage hero-area" style={{backgroundImage: url(${slider1})}}>
-//     <div className="container">
-//       <div className="row">
-//         <div className="col-lg-4 text-center">
-//           <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">DREAMY EXCLUSIVES</p>
-//           <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">GET YOUR SLEEP! </h1>
-//           <p> A Celebration is at Hand! Commemorate the 65th Anniversary of Disney’s Sleeping Beauty with Funko & Loungefly Gear.
-//           <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" className="btn" href="#">Shop Collection</a></p>
-//         </div>
-//         <div className="col-lg-8 banner-img-1">
-//         </div>
-//       </div>
-//     </div>
-//   </div>
+  return (
+    <Slider {...sliderSettings}>
+      <div className="slider-item th-fullpage hero-area" style={{ backgroundImage: `url(${Background1})` }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 text-center">
+              <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">DREAMY EXCLUSIVES</p>
+              <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">GET YOUR SLEEP! </h1>
+              <p> A Celebration is at Hand! Commemorate the 65th Anniversary of Disney's Sleeping Beauty with Funko & Loungefly Gear.
+                <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" className="btn" href="#">Shop Collection</a>
+              </p>
+            </div>
+            <div className="col-lg-8 banner-img-1">
+              {/* Your content for the right side of the slider */}
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="slider-item th-fullpage hero-area" style={{ backgroundImage: `url(${Background1})` }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 text-center">
+              <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">DREAMY EXCLUSIVES</p>
+              <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">GET YOUR SLEEP! </h1>
+              <p> A Celebration is at Hand! Commemorate the 65th Anniversary of Disney's Sleeping Beauty with Funko & Loungefly Gear.
+                <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" className="btn" href="#">Shop Collection</a>
+              </p>
+            </div>
+            <div className="col-lg-8 banner-img-1">
+              {/* Your content for the right side of the slider */}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Add more slider items as needed */}
+    </Slider>
+  );
+}
 
-//   <div className="slider-item th-fullpage hero-area" style={{backgroundImage: url(${require("../assets/images/slider/slider1.jpg")})}}>
-//     <div className="container">
-//       <div className="row">
-//         <div className="col-lg-4 text-center">
-//           <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">DREAMY EXCLUSIVES</p>
-//           <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">GET YOUR SLEEP! </h1>
-//           <p> A Celebration is at Hand! Commemorate the 65th Anniversary of Disney’s Sleeping Beauty with Funko & Loungefly Gear.
-//           <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" className="btn" href="#">Shop Collection</a></p>
-//         </div>
-//         <div className="col-lg-8 banner-img-1">
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-//     </>
-//   )
-// }
-
-// export default Slider
+export default SliderComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import Slider from '../Components/Slider'
+import Slider from '../Components/Slider'
 
 const HomePage = () => {
   return (
     <>
-    {/* <Slider/> */}
-    <h1 className='pt-4 text-info'>HomePage</h1>
+    
+    <Slider/>
     </>
   )
 }
